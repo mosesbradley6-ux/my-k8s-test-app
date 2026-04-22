6290a17ca6bccd03cc111a2cbd18845499e7e052
mosesbradley6-ux <moses.bradley6@gmail.com>
2026-04-22 16:23:03 +0200

    Create vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,10 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "task-tracker"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+}
